cd1cfd36f29003bea1d3724a2f75760633f3e7e3
Ryan Ingram <ry_ingram@hotmail.com>
2016-05-09 23:21:40 -0400

    Applied ColorTypeConverter to project

--- CodePlusXaml/CodePlusXaml/CodePlusXamlPage.xaml.cs
+++ CodePlusXaml/CodePlusXaml/CodePlusXamlPage.xaml.cs
@@ -9,18 +9,28 @@ namespace CodePlusXaml
         {
             InitializeComponent();
 
+            // Demonstrate the ColorTypeConverter
+            var converter = new ColorTypeConverter();
+            var limegreen = Color.FromHex("#00FF00");
+
+
             Label label = new Label
             {
-                Text = "Hello from Code!",
+                Text = "I was changed by ColorTypeConverter",
                 IsVisible = true,
                 Opacity = 0.75,
                 HorizontalTextAlignment = TextAlignment.Center,
                 VerticalOptions = LayoutOptions.CenterAndExpand,
-                TextColor = Color.Blue,
+                // TextColor = Color.Blue,
+                TextColor = limegreen,
                 BackgroundColor = Color.FromRgb(255, 128, 128),
                 FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
                 FontAttributes = FontAttributes.Bold | FontAttributes.Italic
             };
+
+
+
+
 
             (Content as StackLayout).Children.Insert(0, label);
         }
